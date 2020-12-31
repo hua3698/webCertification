a4dd4c06fe129dcc8a8382c695a00479dd82fcb9
hua3698 <a29424315@gmail.com>
2020-12-31 11:42:30 +0800

    pro分支待改

---
+++ web05-no.3/api/save_movie.php
@@ -0,0 +1,27 @@
+<?php 
+include_once "../base.php";
+print_r($_POST);
+
+if(!empty($_FILES['trailer']['tmp_name'])){
+    $_POST['trailer']=$_FILES['trailer']['name'];
+    move_uploaded_file($_FILES['trailer']['tmp_name'],'../img/'.$_FILES['trailer']['name']);
+}
+
+if(!empty($_FILES['poster']['tmp_name'])){
+    $_POST['poster']=$_FILES['poster']['name'];
+    move_uploaded_file($_FILES['poster']['tmp_name'],'../img/'.$_FILES['poster']['name']);
+}
+
+if(isset($_POST['id'])){
+    //修改
+    
+}else{
+    $_POST['sh']=1;
+    $data['rank']=$Movie->q("select max(rank) from movie")[0][0]+1;
+}
+
+
+$Movie->save($_POST);
+
+// 檔案資料庫欄位要設為允許空值
+?>
