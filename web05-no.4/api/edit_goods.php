bd1727bccc6ae4ca145272080fbcac28e68d9cc1
hua3698 <a29424315@gmail.com>
2021-01-29 13:23:32 +0800

    done

--- web05-no.4/api/edit_goods.php
+++ web05-no.4/api/edit_goods.php
@@ -3,6 +3,9 @@ include_once "../base.php";
 
 if(!empty($_FILES['img']['tmp_name'])){
     $_POST['img']=$_FILES['img'][''];
+}else{
+    $good=$Goods->find($_POST['id']);
+    $_POST['img']=$good['img'];
 }
 
 $Goods->save($_POST);
